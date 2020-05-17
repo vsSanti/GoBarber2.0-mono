238a9c6af12ac853ddf920aca875af674cd49c5c
Vinicius Santiago <viniciussdsilva@gmail.com>
2020-05-17 10:51:03 -0300

    Bugfix: can't book past date

--- server/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ server/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,12 +15,16 @@ describe('ListProviderMonthAvailability', () => {
   it('should be able to list day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 17, 8, 0, 0),
+      date: new Date(2020, 4, 17, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 17, 10, 0, 0),
+      date: new Date(2020, 4, 17, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 17, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -33,9 +37,12 @@ describe('ListProviderMonthAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
